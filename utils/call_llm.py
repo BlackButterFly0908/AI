e8f725b7b8f8f2a5b8e900afb8e2c004d606e22e
BlackButterFly0908 <143584637+BlackButterFly0908@users.noreply.github.com>
2025-06-12 17:07:24 +0800

    Update call_llm.py

--- utils/call_llm.py
+++ utils/call_llm.py
@@ -1,18 +1,25 @@
 import os
 from groq import Groq
+import ollama
 
-# Learn more about calling the LLM: https://the-pocket.github.io/PocketFlow/utility_function/llm.html
-def call_llm(prompt):
+
+def call_llm(prompt , llm_server="ollama") -> str:
     """
     Calls the Groq LLM with a given prompt.
     """
-    client = Groq(api_key="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")
-    r = client.chat.completions.create(
-        model="llama-3.1-8b-instant", # Or another suitable Groq model
-        messages=[{"role": "user", "content": prompt}]
-    )
-    return r.choices[0].message.content
-
+    if llm_server == "groq":
+        client = Groq(api_key="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")
+        r = client.chat.completions.create(
+            model="llama-3.1-8b-instant", # Or another suitable Groq model
+            messages=[{"role": "user", "content": prompt}]
+        )
+        return r.choices[0].message.content
+    elif llm_server == "ollama":
+        response = ollama.chat(
+            model="llama3.2:latest" ,
+            messages=[{"role": "user", "content": prompt}],
+        )
+        return response['message']['content']
 if __name__ == "__main__":
     # Example usage
     prompt = "What is the capital of France?"
